a488478d3e93390e0c757ba03fd34666f6dacf4b
osorov97 <osorov.nursultan97@gmail.com>
2022-09-20 17:18:30 +0600

    we2

--- src/Class.java
+++ src/Class.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Class {
+import java.util.Arrays;
+
+public class Class {
+    private int number;
+    private String word;
+    private int [] massive;
+
+    public Class(int number, String word, int[] massive) {
+        this.number = number;
+        this.word = word;
+        this.massive = massive;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public int[] getMassive() {
+        return massive;
+    }
+
+    @Override
+    public String toString() {
+        return "Class{" +
+                "number=" + number +
+                ", word='" + word + '\'' +
+                ", massive=" + Arrays.toString(massive) +
+                '}';
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Class cl = new Class(1,"Bir",new int[] {1,34,45,67,8});
+        System.out.println("Number: " + cl.getNumber());
+        System.out.println("Word: " + cl.getWord());
+
+        for (int i = 0; i <cl.getMassive().length; i++) {
+            System.out.print("Indeks " + i + " : " + cl.getMassive()[i]+ "  ");
+        }
+
     }
 }
